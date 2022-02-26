76021f240b806e298d2b9b4c09408c50f6733eca
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-26 17:11:13 -0600

    Added a separate table for flagged feedback.  Will only render once something is flagged

--- src/components/FlaggedItem/FlaggedItem.jsx
+++ src/components/FlaggedItem/FlaggedItem.jsx
@@ -1,20 +1,25 @@
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
 
-function FlaggedItem() {
+function FlaggedItem({ row, handleFlag, handleDelete }) {
 
-            
+    let callFlag = () => {
+        handleFlag(row.id, row)
+    }
+
+    let callDelete = () => {
+        handleDelete(row.id)
+    }
     return (
         <>
-            
-        <td>{row.feeling}</td>
-        <td>{row.understanding}</td>
-        <td>{row.support}</td>
-        <td>{row.comments}</td>
-        <td><EmojiFlagsIcon onClick={handleFlag}/></td>
-        <td><DeleteSweepIcon onClick={handleDelete}/></td>
-        
-    </>
+            <td>{row.feeling}</td>
+            <td>{row.understanding}</td>
+            <td>{row.support}</td>
+            <td>{row.comments}</td>
+            <td><EmojiFlagsIcon onClick={callFlag}/></td>
+            <td><DeleteSweepIcon onClick={callDelete}/></td>
+
+        </>
     )
 }
 
